61c4a56346558e0df4c98736c9d86fb94ef3b46f
teomerida30@gmail.com <teomerida30@gmail.com>
2024-02-06 15:47:09 +0100

    started topic

--- Topic.cpp
+++ Topic.cpp
@@ -1,7 +1,35 @@
 #include "server.hpp"
 
+// int Server::findChannelByName(std::string name){
+//     std::map<std::string, Channel>::iterator it = _channels.find(name);
+
+//     if (it != _channels.end()) {
+//         return 1;
+//     } else {
+//         std::cout << "Canal no encontrado." << std::endl;
+//     }
+//     return 0;
+// }
+
+
 int Server::cmdTopic(Client *aux, std::vector<std::string> tokens) //Change or view the channel topic
 {
-    std::cout << "TOPIC called" << std::endl;
+    //  if(tokens.size() < 2){
+    //     aux->newMessage(std::string("461 ") + aux->getNick() + tokens[0] + ":Not enough parameters");
+    //     return ;
+    // }
+
+    // //no existe el channel
+    // //no tiene permisos
+    // //creacion del topic
+    // if(ch->getTopic() != ""){
+	// 	aux->newMessage(std::string("332 ") + aux->getNick() + " " + ch->getName() + " " + ch->getTopic());
+	// 	std::cout << std::string("332 ") + aux->getNick() + " " + ch->getName() + " " + ch->getTopic() << std::endl;
+	// 	aux->newMessage(std::string("333 ") + aux->getNick() + " " + ch->getName() + " " + aux->getNick() + " " + _currentTime());
+	// 	std::cout << std::string("333 ") + aux->getNick() + " " + ch->getName() + " " + aux->getNick() + " " + _currentTime() << std::endl;
+	// }else{
+	// 	aux->newMessage(std::string("331 ") + aux->getNick() + " " + ch->getName() + " :No topic is set");
+	// }
     return 0;
+
 }
